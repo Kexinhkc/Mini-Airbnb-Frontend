c29614e0d759938da2148e4c1fb7457c4c8e7ef0
kexin <apple@Kexin.hub>
2023-10-15 10:52:34 +1030

    added tailwind inside Vite config

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from 'tailwindcss'
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: "/Mini-Airbnb-Website/",
+  css: {
+    postcss: {
+      plugins: [tailwindcss],
+    },
+  }
 })
